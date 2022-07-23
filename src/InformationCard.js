8c45027a63a369f6630e6b70ce3f9a8b408e358a
ece <e.akinci@depaulisystems.com>
2022-07-24 00:51:24 +0300

    first commit

---
+++ src/InformationCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ImageCard() {
+    return (
+        <div className='card-container'>
+            <div className='image-container'>
+                <img className='logo' alt='passportPhoto'/>
+            </div>
+            <div className='information-container'>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
